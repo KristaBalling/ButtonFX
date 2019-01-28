fb150a6a012e43e6158ac054b40dd6c9d54cbddc
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-28 10:29:16 -0500

    before lamda

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,21 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+    @FXML
+    private Button clickMeButton;
+
+    public void initialize() {
+        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("You clicked me!");
+            }
+        });
+    }
 }
