e8042a45935d36a56015d4f7517e7901ea109c4a
Krista Blachian <krista.blachian@scifusion360.com>
2019-01-28 10:32:05 -0500

    using lambda

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -11,11 +11,6 @@ public class Controller {
     private Button clickMeButton;
 
     public void initialize() {
-        clickMeButton.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("You clicked me!");
-            }
-        });
+        clickMeButton.setOnAction(event -> System.out.println("You clicked me!"));
     }
 }
